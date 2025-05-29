9157d9b6f99620cc8fbe3a630dfeb2d50c9b1168
tdtc-hrb <tdtc.hrb@gmail.com>
2025-05-28 22:24:53 -0700

    add page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// baseName: com-vc, css-tws, dev-frontend
+export default defineConfig({
+  site: "https://tdtc-hrb.github.io",
+  base: "baseName",
+  trailingSlash: 'ignore',
+});
