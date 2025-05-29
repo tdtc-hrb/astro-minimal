008196595065c553a37878182bd5b5491cdf29e4
tdtc-hrb <tdtc.hrb@gmail.com>
2025-05-28 22:51:31 -0700

    dev-frontend rename blog-frontend

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-// baseName: com-vc, css-tws, dev-frontend
+// baseName: com-vc, css-tws, blog-frontend
 export default defineConfig({
   site: "https://tdtc-hrb.github.io",
   base: "baseName",
