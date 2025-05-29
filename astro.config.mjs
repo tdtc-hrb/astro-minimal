3cb1eefcc0e8a2c8f619dee076298b04332a28d3
tdtc-hrb <tdtc.hrb@gmail.com>
2025-05-29 06:01:06 -0700

    add ops-win

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-// baseName: com-vc, css-tws, blog-frontend
+// baseName: com-vc, css-tws, blog-frontend, ops-win
 export default defineConfig({
   site: "https://tdtc-hrb.github.io",
   base: "baseName",
