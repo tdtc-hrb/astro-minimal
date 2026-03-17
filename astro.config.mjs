a2bff5985e05bc61fc457855816bd85298ff2550
tdtc-hrb <tdtc.hrb@gmail.com>
2026-03-16 20:02:58 -0700

    add modern-cxx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-// baseName: com-vc, css-tws, blog-frontend, ops-win
+// baseName: com-vc, css-tws, blog-frontend, ops-win, modern-cxx
 export default defineConfig({
   site: "https://tdtc-hrb.github.io",
   base: "baseName",
