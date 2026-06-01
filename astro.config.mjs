d7536c47f72e312fc5338e18cd4e499d89a0d410
xiaobin80 <veic_2005@163.com>
2026-05-31 20:25:36 -0700

    add new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-// baseName: com-vc, css-tws, blog-frontend, ops-win, modern-cxx, minifilter
 export default defineConfig({
+  // site: "https://veic-2005.github.io",
   site: "https://tdtc-hrb.github.io",
+  // baseName: com-vc, css-tws, blog-frontend, ops-win, modern-cxx
+  // baseName: minifilter
   base: "baseName",
   trailingSlash: 'ignore',
 });
