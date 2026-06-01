9e11cdda2ddfa9e1b52269e785273fcbb0d23c9d
xiaobin80 <veic_2005@163.com>
2026-05-31 20:26:42 -0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // site: "https://veic-2005.github.io",
+  // baseName: minifilter
   site: "https://tdtc-hrb.github.io",
   // baseName: com-vc, css-tws, blog-frontend, ops-win, modern-cxx
-  // baseName: minifilter
   base: "baseName",
   trailingSlash: 'ignore',
 });
